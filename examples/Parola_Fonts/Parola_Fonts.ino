0b53e7481b340c48e3ca317da1b7ef14c4c77abf
immortalserg <i_m_m@mail.ru>
2018-12-23 22:35:17 +0100

    a msg

--- examples/Parola_Fonts/Parola_Fonts.ino
+++ examples/Parola_Fonts/Parola_Fonts.ino
@@ -3,20 +3,15 @@
 #include <SPI.h>
 #include "5bite_rus.h" // подключаем внешний шрифт
 #define HARDWARE_TYPE MD_MAX72XX::ICSTATION_HW
-#define MAX_DEVICES 2 // количество модулей
-#define CLK_PIN   13 // CLK программного SPI
-#define DATA_PIN  11 // MOSI программного SPI
+#define MAX_DEVICES 4 // количество модулей
 #define CS_PIN    4 // CS SPI
 // Hardware SPI connection
 MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
-// Arbitrary output pins
-// MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
 void setup()
 {
   P.begin();
   P.setFont(_5bite_rus);
-  P.displayText("АБВ", PA_CENTER , 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
-
+  P.displayText("СЪЕШЬ ЕЩЕ ЭТИХ МАЛЕНЬКИХ БУЛОЧЕК И ВЫПЕЙ ЧАЮ, съешь еще этих маленьких булочек и выпей чаю", PA_CENTER , 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
 }
 void loop()
 {
