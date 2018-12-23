9730c4fcb3113fc44a6dec84b83d13826728e918
immortalserg <i_m_m@mail.ru>
2018-12-23 17:25:12 +0100

    a msg

---
+++ examples/Parola_Fonts/Parola_Fonts.ino
@@ -0,0 +1,121 @@
+// Program to demonstrate the MD_Parola library
+//
+// Simplest program that does something useful - Hello World!
+//
+// MD_MAX72XX library can be found at https://github.com/MajicDesigns/MD_MAX72XX
+//
+
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+// #include "Parola_Fonts_data.h"
+#include "5bit_rus.h"
+
+// Define the number of devices we have in the chain and the hardware interface
+// NOTE: These pin numbers will probably not work with your hardware and may
+// need to be adapted
+#define HARDWARE_TYPE MD_MAX72XX::ICSTATION_HW
+#define MAX_DEVICES 2
+#define CLK_PIN   13
+#define DATA_PIN  11
+#define CS_PIN    4
+char *text1;
+
+// Hardware SPI connection
+MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// Arbitrary output pins
+// MD_Parola P = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+String utf8rus(String source)
+{
+  int i,k;
+  String target;
+  unsigned char n;
+  char m[2] = { '0', '\0' };
+  k = source.length(); i = 0;
+  while (i < k) {
+    n = source[i]; i++;
+    if (n >= 0xC0) {
+      switch (n) {
+        case 0xD0: {
+          n = source[i]; i++;
+          if (n == 0x81) { n = 0xA8; break; }
+          if (n >= 0x90 && n <= 0xBF) n = n + 0x30;
+          break;
+        }
+        case 0xD1: {
+          n = source[i]; i++;
+          if (n == 0x91) { n = 0xB8; break; }
+          if (n >= 0x80 && n <= 0x8F) n = n + 0x70;
+          break;
+        }
+      }
+    }
+    m[0] = n; target = target + String(m);
+  }
+return target;
+}
+
+uint8_t utf8Ascii(uint8_t ascii)
+{
+  static uint8_t cPrev;
+  uint8_t c = '\0';
+
+  if (ascii < 0x7f)   // Standard ASCII-set 0..0x7F, no conversion
+  {
+    cPrev = '\0';
+    c = ascii;
+  }
+  else
+  {
+    switch (cPrev)  // Conversion depending on preceding UTF8-character
+    {
+    case 0xC2: c = ascii;  break;
+    case 0xC3: c = ascii | 0xC0;  break;
+    case 0x82: if (ascii==0xAC) c = 0x80; // Euro symbol special case
+    }
+    cPrev = ascii;   // save last char
+  }
+
+//  PRINTX("\nConverted 0x", ascii);
+//  PRINTX(" to 0x", c);
+
+  return(c);
+}
+void utf8Ascii(char* s)
+// In place conversion UTF-8 string to Extended ASCII
+// The extended ASCII string is always shorter.
+{
+  uint8_t c, k = 0;
+  char *cp = s;
+
+//  PRINT("\nConverting: ", s);
+
+  while (*s != '\0')
+  {
+    c = utf8Ascii(*s++);
+    if (c != '\0')
+      *cp++ = c;
+  }
+  *cp = '\0';   // terminate the new string
+}
+
+
+void setup()
+{
+//  text1 = utf8rus("50€/kg³");
+//  utf8Ascii("50€/kg³");
+  P.begin();
+  P.setFont(_5bit_rus);
+  P.displayText("АБВГД", PA_CENTER , 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+
+}
+
+void loop()
+{
+  if (P.displayAnimate())
+  {
+    P.displayReset();
+  }
+
+}
