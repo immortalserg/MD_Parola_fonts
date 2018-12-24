45f710c64cfd7df0dc405f209d9c3b93afc1d1e9
immortalserg <i_m_m@mail.ru>
2018-12-24 01:24:35 +0100

    a msg

--- examples/Parola_Fonts/Parola_Fonts.ino
+++ examples/Parola_Fonts/Parola_Fonts.ino
@@ -2,6 +2,7 @@
 #include <MD_MAX72xx.h>
 #include <SPI.h>
 #include "5bite_rus.h" // подключаем внешний шрифт
+#include "6bite_rus.h"
 #define HARDWARE_TYPE MD_MAX72XX::ICSTATION_HW
 #define MAX_DEVICES 4 // количество модулей
 #define CS_PIN    4 // CS SPI
@@ -10,8 +11,8 @@ MD_Parola P = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 void setup()
 {
   P.begin();
-  P.setFont(_5bite_rus);
-  P.displayText("СЪЕШЬ ЕЩЕ ЭТИХ МАЛЕНЬКИХ БУЛОЧЕК И ВЫПЕЙ ЧАЮ, съешь еще этих маленьких булочек и выпей чаю", PA_CENTER , 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+  P.setFont(_6bite_rus);
+  P.displayText("АБВГД", PA_CENTER , 100, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
 }
 void loop()
 {
